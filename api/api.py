91115fc528df5aa14c038bb737e5a42615580731
athul-v-nair <athulvnair2001@gmail.com>
2026-03-25 00:35:17 +0530

    added top_k to api request

--- api/api.py
+++ api/api.py
@@ -11,6 +11,7 @@ from src.generation.generation import Generator
 class ChatRequest(BaseModel):
     query: str = Field(..., min_length=1, max_length=500, description="User question")
     session_id: str = Field(..., description="Unique Session ID")
+    top_k: int = Field(..., default=3, description="Number of top answers to retrieve")
 
 # FastAPI Setup
 @asynccontextmanager
@@ -63,6 +64,6 @@ async def health():
 async def chat(request: ChatRequest, fastapi_request: Request):
     generator = fastapi_request.app.state.generator
     return StreamingResponse(
-        generator.generate_answer(request.query, request.session_id),
+        generator.generate_answer(request.query, request.session_id, request.top_k),
         media_type="text/plain"
     )   
